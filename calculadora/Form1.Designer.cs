ab874d8fe96e842f308c7f8f94656b3cc0d41938
FellipeGiulianoDuarte <62112999+FellipeGiulianoDuarte@users.noreply.github.com>
2020-03-12 14:15:58 -0300

    Add files via upload

---
+++ calculadora/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tres = new System.Windows.Forms.TextBox();
+            this.cinco = new System.Windows.Forms.TextBox();
+            this.oito = new System.Windows.Forms.TextBox();
+            this.dez = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tres
+            // 
+            this.tres.Location = new System.Drawing.Point(66, 162);
+            this.tres.Name = "tres";
+            this.tres.Size = new System.Drawing.Size(100, 20);
+            this.tres.TabIndex = 0;
+            this.tres.TextChanged += new System.EventHandler(this.tres_TextChanged);
+            // 
+            // cinco
+            // 
+            this.cinco.BackColor = System.Drawing.SystemColors.Window;
+            this.cinco.Location = new System.Drawing.Point(299, 162);
+            this.cinco.Name = "cinco";
+            this.cinco.Size = new System.Drawing.Size(100, 20);
+            this.cinco.TabIndex = 1;
+            this.cinco.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // oito
+            // 
+            this.oito.Location = new System.Drawing.Point(525, 162);
+            this.oito.Name = "oito";
+            this.oito.Size = new System.Drawing.Size(100, 20);
+            this.oito.TabIndex = 2;
+            // 
+            // dez
+            // 
+            this.dez.Location = new System.Drawing.Point(730, 162);
+            this.dez.Name = "dez";
+            this.dez.Size = new System.Drawing.Size(100, 20);
+            this.dez.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(66, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "PACOTE DE 3";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(299, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 13);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "PACOTE DE 5";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(525, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 13);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "PACOTE DE 8";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(730, 107);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 13);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "PACOTE DE 10";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bebas", 36F);
+            this.button1.Location = new System.Drawing.Point(66, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "-5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Bebas", 36F);
+            this.button2.Location = new System.Drawing.Point(299, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "-1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Bebas", 36F);
+            this.button3.Location = new System.Drawing.Point(525, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "+1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Bebas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(730, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "+5";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 418);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dez);
+            this.Controls.Add(this.oito);
+            this.Controls.Add(this.cinco);
+            this.Controls.Add(this.tres);
+            this.Name = "Form1";
+            this.Text = "CALCULADORA GELO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tres;
+        private System.Windows.Forms.TextBox cinco;
+        private System.Windows.Forms.TextBox oito;
+        private System.Windows.Forms.TextBox dez;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
